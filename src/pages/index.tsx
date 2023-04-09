da9b4b30ba2399bb41edf5dbe640fae88c93c285
Steven Enriquez <stevenenriquez123@gmail.com>
2023-04-09 15:42:15 -0700

    description update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
         </h1>
         <Image className="rounded-full border-4 border-gray-800 w-max mt-6" src="https://github.com/stevenenriquez.png?size=300" alt="Steven Enriquez" width={300} height={300} />
         <p className="text-white text-xl sm:text-2xl mt-12 sm:mt-24 mb-12 lg:w-1/3 md:w-1/2 w-3/4 text-center">
-          I&apos;m a full stack web developer from Arizona 🌅 <br/><br/>I&apos;ll be showcasing my personal projects, photography, and also writing about topics that interest me. If you&apos;d like, follow me on any of the social media linked below. Thanks for stopping by.
+          I&apos;m a full stack web developer from Arizona 🌅 <br/><br/>I&apos;ll be showcasing personal projects, photography, and also writing about topics that interest me. If you&apos;d like, follow me on any of the platforms linked below. Thanks for stopping by.
         </p>
         <SocialLinks />
       </main>
